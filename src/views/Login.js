5df268429b62d835d30ac479be1918c5e982b87a
Williamcsa04 <williamcaetano14@gmail.com>
2018-10-02 02:46:00 -0300

    Changed placeholder of login

--- src/views/Login.js
+++ src/views/Login.js
@@ -9,8 +9,10 @@ class Login extends Component {
                     <Col>
                         <Form>
                             <FormGroup>
-                            	<Label for="exampleEmail">Email</Label>
-                                <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+                            	<Label for="emailLogin">Email</Label>
+                                <Input type="email" name="email" id="emailLogin" placeholder="email@mail.com" />
+								<Label for="passwordLogin">Password</Label>
+          						<Input type="password" name="password" id="passwordLogin" placeholder="Senha" />
                             </FormGroup>
                         </Form>
                     </Col>
