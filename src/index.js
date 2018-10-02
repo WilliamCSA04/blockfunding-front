534490446cfab377258b67367590161993fd06d1
William Caetano <williamcaetano14@gmail.com>
2018-10-01 23:50:52 -0300

    Added logic to router

--- src/index.js
+++ src/index.js
@@ -3,7 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import NotFound from './NotFound';
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" exact={true} component={App} />
+            <Route path="/404" component={NotFound} />
+        </Switch>
+    </ BrowserRouter>
+    , document.getElementById('root'));
 registerServiceWorker();
