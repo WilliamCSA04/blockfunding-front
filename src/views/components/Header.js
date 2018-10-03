a050e77f1d47b583dbb8e1c9515d5fef46800c86
William Caetano <williamcaetano14@gmail.com>
2018-10-02 22:51:16 -0300

    Created header

---
+++ src/views/components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Navbar } from 'reactstrap';
+
+class Header extends Component {
+
+    render() {
+        return (
+            <Navbar color="light" light expand="md">
+
+            </Navbar>
+        );
+    }
+}
+
+export default Header;
