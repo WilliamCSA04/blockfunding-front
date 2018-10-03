bc1fd48d14a8f1670e67922ac38f511679de9da9
William Caetano <williamcaetano14@gmail.com>
2018-10-03 00:57:05 -0300

    Changed login page

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,31 +1,65 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
-import { Container, Row, Col, Form, FormGroup, Label, Input, Button } from 'reactstrap';
 import Header from './components/Header';
+import { Button, Card, CardBody, CardGroup, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
+
+
 
 class Login extends Component {
     render() {
         return (
-            <React.Fragment>
-                <Header />
+            <div className="app flex-row align-items-center">
                 <Container>
-                    <Row>
-                        <Col md={{size:6, offset: 3}}>
+                <Row className="justify-content-center">
+                    <Col md="8">
+                    <CardGroup>
+                        <Card className="p-4">
+                        <CardBody>
                             <Form>
-                                <FormGroup>
-                                    <Label for="emailLogin">Email</Label>
-                                    <Input type="email" name="email" id="emailLogin" placeholder="email@mail.com" />
-                                    <Label for="passwordLogin">Password</Label>
-                                    <Input type="password" name="password" id="passwordLogin" placeholder="Senha" />
-                                </FormGroup>
-                                <Button color="primary float-right">Login</Button>
-                                <Link to="/register"><Button outline color="primary">Registrar</Button></Link>
+                            <h1>Login</h1>
+                            <p className="text-muted">Sign In to your account</p>
+                            <InputGroup className="mb-3">
+                                <InputGroupAddon addonType="prepend">
+                                <InputGroupText>
+                                    <i className="icon-user"></i>
+                                </InputGroupText>
+                                </InputGroupAddon>
+                                <Input type="text" placeholder="Username" autoComplete="username" />
+                            </InputGroup>
+                            <InputGroup className="mb-4">
+                                <InputGroupAddon addonType="prepend">
+                                <InputGroupText>
+                                    <i className="icon-lock"></i>
+                                </InputGroupText>
+                                </InputGroupAddon>
+                                <Input type="password" placeholder="Password" autoComplete="current-password" />
+                            </InputGroup>
+                            <Row>
+                                <Col xs="6">
+                                <Button color="primary" className="px-4">Login</Button>
+                                </Col>
+                                <Col xs="6" className="text-right">
+                                <Button color="link" className="px-0">Forgot password?</Button>
+                                </Col>
+                            </Row>
                             </Form>
-                        </Col>
-                    </Row>
+                        </CardBody>
+                        </Card>
+                        <Card className="text-white bg-primary py-5 d-md-down-none" style={{ width: 44 + '%' }}>
+                        <CardBody className="text-center">
+                            <div>
+                            <h2>Sign up</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                                labore et dolore magna aliqua.</p>
+                            <Button color="primary" className="mt-3" active>Register Now!</Button>
+                            </div>
+                        </CardBody>
+                        </Card>
+                    </CardGroup>
+                    </Col>
+                </Row>
                 </Container>
-            </React.Fragment>
-
+            </div>
         );
     }
 }
