403b1f0544b3dfe18eeb9b92f4b9ebc5c7508560
William Caetano <williamcaetano14@gmail.com>
2018-10-02 23:34:40 -0300

    Added header to login

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import { Container, Row, Col, Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import Header from './components/Header';
 
 class Login extends Component {
     render() {
         return (
-            <Container>
-                <Row>
-                    <Col md={{size:6, offset: 3}}>
-                        <Form>
-                            <FormGroup>
-                            	<Label for="emailLogin">Email</Label>
-                                <Input type="email" name="email" id="emailLogin" placeholder="email@mail.com" />
-								<Label for="passwordLogin">Password</Label>
-          						<Input type="password" name="password" id="passwordLogin" placeholder="Senha" />
-                            </FormGroup>
-							<Button color="primary float-right">Login</Button>
-							<Link to="/register"><Button outline color="primary">Registrar</Button></Link>
-                        </Form>
-                    </Col>
-                </Row>
-            </Container>
+            <React.Fragment>
+                <Header />
+                <Container>
+                    <Row>
+                        <Col md={{size:6, offset: 3}}>
+                            <Form>
+                                <FormGroup>
+                                    <Label for="emailLogin">Email</Label>
+                                    <Input type="email" name="email" id="emailLogin" placeholder="email@mail.com" />
+                                    <Label for="passwordLogin">Password</Label>
+                                    <Input type="password" name="password" id="passwordLogin" placeholder="Senha" />
+                                </FormGroup>
+                                <Button color="primary float-right">Login</Button>
+                                <Link to="/register"><Button outline color="primary">Registrar</Button></Link>
+                            </Form>
+                        </Col>
+                    </Row>
+                </Container>
+            </React.Fragment>
+
         );
     }
 }
