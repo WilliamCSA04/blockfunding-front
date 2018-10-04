2a8247dbd0d0360ad7308f60d922be2a9b281c98
Williamcsa04 <williamcaetano14@gmail.com>
2018-10-04 20:25:49 -0300

    Removed not used code from Header

--- src/views/components/Header.js
+++ src/views/components/Header.js
@@ -11,26 +11,14 @@ import {
 } from 'reactstrap';
 
 class Header extends Component {
-    constructor(props) {
-        super(props);
 
-        this.toggle = this.toggle.bind(this);
-        this.state = {
-            isOpen: false
-        };
-    }
-    toggle() {
-        this.setState({
-            isOpen: !this.state.isOpen
-        });
-    }
     render() {
         return (
             <Navbar color="dark" light expand="md">
                 <Container>
                     <NavbarBrand href="/" className="text-white">Blockfunding</NavbarBrand>
                     <NavbarToggler onClick={this.toggle} />
-                    <Collapse isOpen={this.state.isOpen} navbar>
+                    <Collapse navbar>
                         <Nav className="ml-auto" navbar>
                             <NavItem>
                                 <NavLink href="/login" className="text-white">Login</NavLink>
