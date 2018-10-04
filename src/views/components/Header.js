a10c9e426c410fcab49578379c69729979739566
Felipe Castro <fcastro@opsolve.com>
2018-10-04 17:28:41 -0300

    Improved header

--- src/views/components/Header.js
+++ src/views/components/Header.js
@@ -1,20 +1,45 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import { 
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink
+} from 'reactstrap';
 
 class Header extends Component {
+    constructor(props) {
+        super(props);
 
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
     render() {
         return (
             <Navbar color="dark" light expand="md">
-                <NavbarBrand href="/" className="text-white">Blockfunding</NavbarBrand>
-                <Nav className="ml-auto" navbar>
-                    <NavItem>
-                        <NavLink href="/login" className="text-white">Login</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="/register" className="text-white">Registrar</NavLink>
-                    </NavItem>
-                </Nav>
+                <div className="container">
+                    <NavbarBrand href="/" className="text-white">Blockfunding</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="/login" className="text-white">Login</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="/register" className="text-white">Registrar</NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </div>
             </Navbar>
         );
     }
