29b9c833a11d491b032f52b19e39ea02fc858614
Felipe Castro <fcastro@opsolve.com>
2018-10-09 18:21:39 -0300

    Projects list typo fixes

--- src/views/components/projects/Projects.js
+++ src/views/components/projects/Projects.js
@@ -24,7 +24,7 @@ class Projects extends Component {
                                         <img src="https://zdnet4.cbsistatic.com/hub/i/2018/04/13/36c52953-7ab9-4608-a848-71d1d538856e/2cad00bf6c3dc3ff21009836b989cda7/td-deep-learning.jpg" />
                                         <div className="project-detail">
                                             <h1 className="project-title">IA Project</h1>
-                                            <p className="project-small-desc">Help-me make a IA to identify payment frauds.</p>
+                                            <p className="project-small-desc">Help me make a AI to identify payment frauds.</p>
                                             <div className="project-info">
                                                 <div><i className="fa fa-users"></i> 110 Investors</div>
                                                 <div><i className="fa fa-money"></i> 920</div>
@@ -81,8 +81,8 @@ class Projects extends Component {
                                             <h1 className="project-title">IA Project</h1>
                                             <p className="project-small-desc">Help-me make a IA to identify payment frauds.</p>
                                             <div className="project-info">
-                                                <div><i className="fa fa-users"></i> Investors</div>
-                                                <div><i className="fa fa-money"></i> 500</div>
+                                                <div><i className="fa fa-users"></i> 110 Investors</div>
+                                                <div><i className="fa fa-money"></i> 920 </div>
                                             </div>
                                         </div>
                                     </div>
