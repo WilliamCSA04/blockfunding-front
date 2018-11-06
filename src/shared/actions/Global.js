b9270d22df27737f7e69bdb4b80f075f1fa47485
Williamcsa04 <williamcaetano14@gmail.com>
2018-11-06 21:31:24 -0200

    Added global consts to ports

--- src/shared/actions/Global.js
+++ src/shared/actions/Global.js
@@ -1,1 +1,4 @@
-export const env = "http://ec2-18-220-169-89.us-east-2.compute.amazonaws.com"
+export const env = "http://18.224.212.231"
+export const portProject = ":8081"
+export const portAuth = ":8082"
+export const portWallet = ":8083"
