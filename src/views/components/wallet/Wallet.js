81b07355ca99295fc935c48835d3c59419fe6368
Williamcsa04 <williamcaetano14@gmail.com>
2018-11-15 13:59:55 -0200

    Created wallet

---
+++ src/views/components/wallet/Wallet.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import Register from '../../components/Register';
+import { Button, CardBody, CardHeader, FormGroup, Input, Label, Form } from 'reactstrap';
+
+class WalletRegister extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: "",
+            description: "",
+        }
+    }
+
+
+    onChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value })
+    }
+
+    onClick = (event) => {
+        event.preventDefault();
+        const body = {
+            name: this.state.name,
+            description: this.state.description,
+            owner: 1,
+            type: "PERSONAL"
+        }
+    }
+
+    render() {
+        return (
+            <Register>
+                <CardHeader>
+                    <strong>Wallet</strong>
+                </CardHeader>
+                <CardBody>
+                    <Form>
+                        <FormGroup>
+                            <Label htmlFor="Wallet">Wallet Name</Label>
+                            <Input type="text" id="wallet" placeholder="Type a name to your wallet" name="name" onChange={this.onChange} />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label htmlFor="vat">Wallet Description</Label>
+                            <Input type="textarea" id="vat" placeholder="Describe your wallet" name="description" onChange={this.onChange} />
+                        </FormGroup>
+                        <div className="form-actions">
+                            <Button type="submit" color="primary" onClick={this.onClick}>Register Wallet</Button>
+                        </div>
+                    </Form>
+                </CardBody>
+            </Register>
+        );
+    }
+}
+
+export default WalletRegister;
