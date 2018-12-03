d904a3ee74ac63a32cefece8c006c60ba6b401bc
Williamcsa04 <williamcaetano14@gmail.com>
2018-12-03 21:17:53 -0200

    Added logic to get register a user

--- src/shared/actions/Login.js
+++ src/shared/actions/Login.js
@@ -1,6 +1,34 @@
 import axios from "axios";
 import { getEnv } from "./Global";
 
+export function apikey(){
+    if(sessionStorage.token === undefined){
+        const config = {
+            headers: {
+              "Accept": "application/json",
+              "Authorization": "Basic YXBpbWFzdGVyOmFwaXNlY3JldA==",
+              "Content-Type": "application/x-www-form-urlencoded",
+            }
+          };
+        return axios.post(`${getEnv("auth")}/oauth/token?grant_type=client_credentials`, {}, config).then(({ data }) => {
+            sessionStorage.token = JSON.stringify(data)
+        })
+    }
+}
+
 export function login(body){
-    return axios.post(`${getEnv("auth")}/router/auth/oauth/token`, body)
+    apikey();
+    return axios.post(`${getEnv("auth")}/oauth/token`, body)
+}
+
+export function create(body){
+    apikey();
+    let token = JSON.parse(sessionStorage.token)
+    const config = {
+        headers: {
+            Authorization: `bearer ${token.access_token}`
+        }
+      };
+      console.log(config)
+    return axios.post(`${getEnv("auth")}/user`, body, config)
 }
--- src/views/components/users/UserRegister.js
+++ src/views/components/users/UserRegister.js
@@ -2,8 +2,31 @@ import React, { Component } from 'react';
 import { Button, CardBody, CardFooter, Col, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import Register from '../../components/Register.js';
+import { create } from '../../../shared/actions/Login'
 
 class UserRegister extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      username: "",
+      email: "",
+      password: "",
+      name: "",
+    }
+  }
+
+  onChange = (e) => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  onClick = (e) => {
+    e.preventDefault();
+    create(this.state).then(({data}) => {
+      console.log(data);
+    })
+  }
+
   render() {
     return (
         <Register>
@@ -11,37 +34,37 @@ class UserRegister extends Component {
             <Form>
               <h1>Sign up</h1>
               <p className="text-muted">Register your account</p>
-              <InputGroup className="mb-3">
-                <InputGroupAddon addonType="prepend">
-                  <InputGroupText>
-                    <i className="icon-user"></i>
-                  </InputGroupText>
-                </InputGroupAddon>
-                <Input type="text" placeholder="Username" autoComplete="username" />
-              </InputGroup>
-              <InputGroup className="mb-3">
-                <InputGroupAddon addonType="prepend">
-                  <InputGroupText>@</InputGroupText>
-                </InputGroupAddon>
-                <Input type="email" placeholder="Email" autoComplete="email" />
-              </InputGroup>
-              <InputGroup className="mb-3">
-                <InputGroupAddon addonType="prepend">
-                  <InputGroupText>
-                    <i className="icon-lock"></i>
-                  </InputGroupText>
-                </InputGroupAddon>
-                <Input type="password" placeholder="Password" autoComplete="new-password" />
-              </InputGroup>
               <InputGroup className="mb-4">
                 <InputGroupAddon addonType="prepend">
                   <InputGroupText>
                     <i className="icon-lock"></i>
                   </InputGroupText>
                 </InputGroupAddon>
-                <Input type="password" placeholder="Confirm Password" autoComplete="new-password" />
+                <Input type="text" name="name" placeholder="name" autoComplete="name" onChange={this.onChange}/>
               </InputGroup>
-              <Button color="success" block>Sign up</Button>
+              <InputGroup className="mb-3">
+                <InputGroupAddon addonType="prepend">
+                  <InputGroupText>
+                    <i className="icon-user"></i>
+                  </InputGroupText>
+                </InputGroupAddon>
+                <Input type="text" name="username" placeholder="Username" autoComplete="username" onChange={this.onChange}/>
+              </InputGroup>
+              <InputGroup className="mb-3">
+                <InputGroupAddon addonType="prepend">
+                  <InputGroupText>@</InputGroupText>
+                </InputGroupAddon>
+                <Input type="email" name="email" placeholder="Email" autoComplete="email" onChange={this.onChange}/>
+              </InputGroup>
+              <InputGroup className="mb-3">
+                <InputGroupAddon addonType="prepend">
+                  <InputGroupText>
+                    <i className="icon-lock"></i>
+                  </InputGroupText>
+                </InputGroupAddon>
+                <Input type="password" name="password" placeholder="Password" autoComplete="new-password" onChange={this.onChange}/>
+              </InputGroup>
+              <Button color="success" onClick={this.onClick} block>Sign up</Button>
             </Form>
           </CardBody>
           <CardFooter className="p-4">
