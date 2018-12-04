9d42ff4db098de6f4f314206bdc95cbefc31ac8d
Williamcsa04 <williamcaetano14@gmail.com>
2018-12-03 22:55:33 -0200

    Show projects from a specific member

--- src/views/components/account/Account.js
+++ src/views/components/account/Account.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import { Col, Container, Row } from 'reactstrap';
+import { Link } from 'react-router-dom';
 import { read as readProject } from '../../../shared/actions/Project'
 import { read as readWallet } from '../../../shared/actions/Wallet'
+import ProjectList from '../projects/ProjectsList'
 
 class Account extends Component {
 
     constructor(props){
         super(props);
         this.state = {
-            name: "",
-            neededFunds: 0,
-            description: ""
+            wallets: [],
+            projects: []
         }
     }
     
@@ -30,10 +31,26 @@ class Account extends Component {
     
 
     render() {
+        const projects = this.state.projects.map(project => {
+            return <ProjectList title={project.name} description={project.description} neededFunds={project.neededFunds} />
+        })
         return (
-            <div>
-                
-            </div>
+            <React.Fragment>
+      <div className="bg-white projects recent-project">
+
+        <Container>
+          <div className="projects-header">
+            <h1 className="projects-title">My projects</h1>
+            <Link to="/projects/recent">See more...</Link>
+          </div>
+          <div className="projects-content">
+            <Row>
+                {projects}
+                </Row>
+          </div>
+        </Container>
+      </div>
+            </React.Fragment>
         );
     }
 }
